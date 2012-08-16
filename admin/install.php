c9b80816182487940473f7cd0a946576543f066e
Asika <asika32764@gmail.com>
2012-08-16 15:52:18 +0800

    install script test 6

--- admin/install.php
+++ admin/install.php
@@ -15,7 +15,49 @@ class com_AkquickiconsInstallerScript
 	 */
 	function install($parent) 
 	{
+		jimport('joomla.filesystem.folder');
+		$origin = $path.DS.'admin'.DS.'images' ;
+		$target = JPATH_ROOT ;
 		
+		JFolder::move($origin,$target);
+		
+		// set Category
+		$q = $db->getQuery(true) ;
+		
+		$q->select('id')
+			->from('#__categories')
+			->where("extension = 'com_akquickicons'")
+			;
+		
+		$db->setQuery($q);
+		$catids = $db->loadResultArray();
+		
+		$cat = JTable::getInstance('Category') ;
+		
+		foreach( $catids as $catid ):
+			$cat->load($catid);
+			$cat->store();
+		endforeach;
+		
+		// set icons
+		$q = $db->getQuery(true) ;
+		
+		$q->select('id')
+			->from('#__akquickicons_icons')
+			;
+		
+		$db->setQuery($q);
+		$icon_ids = $db->loadResultArray();
+		
+		$table_path = $path.DS.'admin'.DS.'tables' ;
+		JTable::addIncludePath($table_path);
+		$icon = JTable::getInstance('icon', 'AkquickiconsTable') ;
+		
+		foreach( $icon_ids as $icon_id ):
+			$icon-load($icon_id);
+			$icon->catid = $cat_ids[0] ;
+			$icon->store();
+		endforeach;
 	}
  
 	/**
@@ -73,49 +115,7 @@ class com_AkquickiconsInstallerScript
 		$path = $p_installer->getPath('source');
 		
 		if($type == 'install') {
-			jimport('joomla.filesystem.folder');
-			$origin = JPATH_ADMINISTRATOR.DS.'components'.DS.'com_akquickicons'.DS.'images' ;
-			$target = JPATH_ROOT.DS ;
 			
-			JFolder::move($origin,$target);
-			
-			// set Category
-			$q = $db->getQuery(true) ;
-			
-			$q->select('id')
-				->from('#__categories')
-				->where("extension = 'com_akquickicons'")
-				;
-			
-			$db->setQuery($q);
-			$catids = $db->loadResultArray();
-			
-			$cat = JTable::getInstance('Category') ;
-			
-			foreach( $catids as $catid ):
-				$cat->load($catid);
-				$cat->store();
-			endforeach;
-			
-			// set icons
-			$q = $db->getQuery(true) ;
-			
-			$q->select('id')
-				->from('#__akquickicons_icons')
-				;
-			
-			$db->setQuery($q);
-			$icon_ids = $db->loadResultArray();
-			
-			$table_path = $path.DS.'admin'.DS.'tables' ;
-			JTable::addIncludePath($table_path);
-			$icon = JTable::getInstance('icon', 'AkquickiconsTable') ;
-			
-			foreach( $icon_ids as $icon_id ):
-				$icon-load($icon_id);
-				$icon->catid = $cat_ids[0] ;
-				$icon->store();
-			endforeach;
 		}
 		
 		// install module
