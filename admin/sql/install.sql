a62668f1da58413e3d118a3b61a0ec9de50bd4f7
Asika <asika32764@gmail.com>
2012-08-16 18:02:41 +0800

    install script test 10

--- admin/sql/install.sql
+++ admin/sql/install.sql
@@ -1,11 +1,6 @@
 CREATE TABLE IF NOT EXISTS `#__akquickicons_icons` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `asset_id` int(11) NOT NULL,
-  `parent_id` int(10) unsigned NOT NULL,
-  `lft` int(11) NOT NULL,
-  `rgt` int(11) NOT NULL,
-  `level` int(10) unsigned NOT NULL,
-  `path` varchar(255) NOT NULL,
   `catid` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `alias` varchar(255) NOT NULL,
@@ -43,7 +38,7 @@ INSERT INTO `#__categories` (`parent_id`, `lft`, `rgt`, `level`, `path`, `extens
 (1, 135, 136, 1, 'core-icons', 'com_akquickicons', 'Core Icons', 'core-icons', '', '', 1, 0, '0000-00-00 00:00:00', 1, '{"category_layout":"","image":""}', '', '', '{"author":"","robots":""}', 908, '2012-08-12 04:03:49', 908, '2012-08-12 04:05:29', 0, '*');
 
 -- Icons
-INSERT INTO `#__akquickicons_icons` (`parent_id`, `lft`, `rgt`, `level`, `path`, `catid`, `title`, `alias`, `link`, `introtext`, `fulltext`, `images`, `version`, `created`, `created_by`, `modified`, `modified_by`, `ordering`, `published`, `publish_up`, `publish_down`, `checked_out`, `checked_out_time`, `access`, `language`, `params`) VALUES
-(0, 0, 0, 0, '', 80, 'New Article', 'new-article', 'index.php?option=com_content&task=article.add', '', '', 'images/quickicons/Primo-Icons/blog_add_48.png', 0, '2012-08-12 04:03:16', 908, '0000-00-00 00:00:00', 0, 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 1, '*', ''),
-(0, 0, 0, 0, '', 80, 'Article Manager', 'article-manager', 'index.php?option=com_content', '', '', 'images/quickicons/Primo-Icons/blog_compose_48.png', 0, '2012-08-12 04:09:56', 908, '0000-00-00 00:00:00', 0, 2, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 1, '*', ''),
-(0, 0, 0, 0, '', 80, 'Category Manager', 'category-manager', 'index.php?option=com_categories&extension=com_content', '', '', 'images/quickicons/Primo-Icons/bookmark_48.png', 0, '2012-08-12 04:14:42', 908, '0000-00-00 00:00:00', 0, 3, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 1, '*', '');
+INSERT INTO `#__akquickicons_icons` (`catid`, `title`, `alias`, `link`, `introtext`, `fulltext`, `images`, `version`, `created`, `created_by`, `modified`, `modified_by`, `ordering`, `published`, `publish_up`, `publish_down`, `checked_out`, `checked_out_time`, `access`, `language`, `params`) VALUES
+(80, 'New Article', 'new-article', 'index.php?option=com_content&task=article.add', '', '', 'images/quickicons/Primo-Icons/blog_add_48.png', 0, '2012-08-12 04:03:16', 908, '0000-00-00 00:00:00', 0, 1, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 1, '*', ''),
+(80, 'Article Manager', 'article-manager', 'index.php?option=com_content', '', '', 'images/quickicons/Primo-Icons/blog_compose_48.png', 0, '2012-08-12 04:09:56', 908, '0000-00-00 00:00:00', 0, 2, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 1, '*', ''),
+(80, 'Category Manager', 'category-manager', 'index.php?option=com_categories&extension=com_content', '', '', 'images/quickicons/Primo-Icons/bookmark_48.png', 0, '2012-08-12 04:14:42', 908, '0000-00-00 00:00:00', 0, 3, 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00', 1, '*', '');
