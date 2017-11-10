2951613859b0953618a031de472e2ff22075fb02
Simon Asika <asika32764@gmail.com>
2017-11-10 10:33:16 +0800

    New scripts

---
+++ bin/dirs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Part of Component Ezset files.
+ *
+ * @copyright   Copyright (C) 2017 Asikart.co. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+
+const _JEXEC = 1;
+defined('_JEXEC') or die;
+
+return array(
+	'administrator/components/com_akquickicons' => 'admin',
+    'administrator/modules/mod_akquickicons' => 'modules/mod_akquickicons',
+);
